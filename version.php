8c417ff9fd31e95a318a3c1aef633f4a982d4329
Marcus Green <marcusavgreen@gmail.com>
2025-07-22 08:26:56 +0100

    Changed order of adding spellcheck fields to upgrades as it was being missed

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_aitext';
-$plugin->version   = 2025071200;
+$plugin->version   = 2025072200;
 $plugin->requires  = 2020110900;
-$plugin->release = '0.03';
+$plugin->release = '0.04';
 $plugin->maturity  = MATURITY_BETA;
